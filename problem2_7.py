d8cc8fbd3180eb8983c510c3b58e00c6d2b4729e
uditi-12 <72464340+uditi-12@users.noreply.github.com>
2021-11-12 22:41:31 +0530

    Delete problem2_7.py

--- problem2_7.py
+++
@@ -1,8 +0,0 @@
-def problem2_7():
-    a=float(input("Enter length of side one: "))
-    b=float(input("Enter length of side two: "))
-    c=float(input("Enter length of side three: "))
-    s=(0.5)*(a+b+c)
-    print("Area of a triangle with sides",a,b,c,"is",(s*(s-a)*(s-b)*(s-c))**(1/2))
-
-
